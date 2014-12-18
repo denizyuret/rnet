9bee1bb79d6f412037090eb71fc61d1da769e315
Deniz Yuret <denizyuret@gmail.com>
2014-12-18 09:46:23 +0200

    Take log after sumind and avoid -inf using realmin.

--- softmax_diff.m
+++ softmax_diff.m
@@ -2,7 +2,6 @@ function [dz, J] = softmax_diff(z, y)
     p = softmax(gather(z));             % works better on cpu
     Y = full(sparse(double(gather(y)), 1:numel(y), 1));
     dz = (p - Y) / size(p, 2);
-    logp = log(p);
-    cost = logp(sub2ind(size(logp), y, 1:numel(y)));
-    J = -mean(cost);
+    py = p(sub2ind(size(p), y, 1:numel(y)));
+    J = -mean(log(max(py, realmin(class(py)))));
 end
