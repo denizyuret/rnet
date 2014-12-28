d9ebf9c0d0edf02c58dab12f8398ab2d2dec2150
Deniz Yuret <denizyuret@gmail.com>
2014-12-28 22:36:20 +0200

    Eliminate forward_batch.

--- evalnet.m
+++ evalnet.m
@@ -1,5 +1,15 @@
 function [accuracy, cost] = evalnet(net, x, y)
-    p = forward_batch(net, x, 1000);
+    m = size(x, 2);
+    b = 1000;
+    p = [];
+    for i=1:b:m
+        j = min(i+b-1, m);
+        xij = x(:,i:j);
+        for l=1:numel(net)
+            xij = net{l}.forw(xij);
+        end
+        p = [p, xij];
+    end
     py = p(sub2ind(size(p), y, 1:numel(y)));
     cost = -mean(log(max(py, realmin(class(py)))));
     [~,h] = max(p);
