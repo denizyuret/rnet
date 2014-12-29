123a18570b22c9634fc47020fef7f7d6a7c99fc3
Deniz Yuret <denizyuret@gmail.com>
2014-12-29 23:19:02 +0200

    Added loss and accuracy methods.

--- soft.m
+++ soft.m
@@ -11,6 +11,16 @@ classdef soft < layer
             dy = (l.y - dy) / m;
         end
 
+        function nll = loss(l, probs, labels)
+            py = probs(sub2ind(size(probs), labels, 1:numel(labels)));
+            nll = -mean(log(max(py, realmin(class(py)))));
+        end
+
+        function acc = accuracy(l, probs, labels)
+            [~,maxp] = max(probs);
+            acc = mean(maxp == labels);
+        end
+
         function l = soft(varargin)
             l = l@layer(varargin{:});
         end
