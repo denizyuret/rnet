8e2e26fa41f42690d89366cc6cfadb2b65ade1b0
Deniz Yuret <denizyuret@gmail.com>
2014-12-29 23:18:48 +0200

    The work should be done by forward and the net.

--- evalnet.m
+++ evalnet.m
@@ -1,17 +1,5 @@
-function [accuracy, cost] = evalnet(net, x, y)
-    m = size(x, 2);
-    b = 1000;
-    p = [];
-    for i=1:b:m
-        j = min(i+b-1, m);
-        xij = x(:,i:j);
-        for l=1:numel(net)
-            xij = net{l}.forw(xij);
-        end
-        p = [p, xij];
-    end
-    py = p(sub2ind(size(p), y, 1:numel(y)));
-    cost = -mean(log(max(py, realmin(class(py)))));
-    [~,h] = max(p);
-    accuracy = mean(h==y);
+function [accuracy, loss] = evalnet(net, x, y)
+    p = forward(net, x, 1000);
+    accuracy = net{end}.accuracy(p, y);
+    loss = net{end}.loss(p, y);
 end
