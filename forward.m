c65628386dda2f560464f35d2806e0718217d1b7
Deniz Yuret <denizyuret@gmail.com>
2014-12-30 18:47:11 +0200

    Concatenate instead of pre-allocate!

--- forward.m
+++ forward.m
@@ -1,14 +1,13 @@
 function y = forward(net, x, batch)
     ncol = size(x, 2);
     if nargin < 3 batch = ncol; end
-    nrow = size(net{end}.w, 1);
-    y = zeros(nrow, ncol, 'like', x);
+    y = [];
     for i=1:batch:ncol
         j = min(i+batch-1, ncol);
         a = x(:,i:j);
         for l=1:numel(net)
             a = net{l}.forw(a);
         end
-        y(:,i:j) = a;
+        y = [y, a];
     end
 end
