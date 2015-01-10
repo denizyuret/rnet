1a8a1ac7db1c1bf12e422495b9d82d2e710eb299
Deniz Yuret <denizyuret@gmail.com>
2015-01-10 10:15:52 +0200

    Added optional argument for batch size.

--- evalnet.m
+++ evalnet.m
@@ -1,5 +1,8 @@
-function [accuracy, loss] = evalnet(net, x, y)
-    p = forward(net, x, 1000);
+function [accuracy, loss] = evalnet(net, x, y, batch)
+    if nargin < 4
+        batch = 1000;
+    end
+    p = forward(net, x, batch);
     accuracy = net{end}.accuracy(p, y);
     loss = net{end}.loss(p, y);
 end
